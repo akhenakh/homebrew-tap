5988244c3653deba6d7d4cc824683e589a581084
Fabrice Aneche <akh@inair.space>
2026-08-24 14:02:24 +0000

    Brew formula update for vduck v1.2.4

--- Formula/vduck.rb
+++ Formula/vduck.rb
@@ -8,23 +8,23 @@ class Vduck < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/akhenakh/vduck/releases/download/v1.2.3/vduck_Darwin_arm64.tar.gz"
-      sha256 "cc609f4e1253ec296407d077945bd5caa472aeb48a3c11d78a7df24b8844731d"
+      url "https://github.com/akhenakh/vduck/releases/download/v1.2.4/vduck_Darwin_arm64.tar.gz"
+      sha256 "8bac67b46a5447d4f56408827fa72ad7aa54ed1f5510fd1bb3163f1b1e9d9a74"
     end
     on_intel do
-      url "https://github.com/akhenakh/vduck/releases/download/v1.2.3/vduck_Darwin_x86_64.tar.gz"
-      sha256 "77dfb283642b55cef406bdf235155eea2c04ee5ed2b6f83d72696914f149ca52"
+      url "https://github.com/akhenakh/vduck/releases/download/v1.2.4/vduck_Darwin_x86_64.tar.gz"
+      sha256 "689858553256df3bbd5c70efc9b93ce7abd657b645372368a914579e5d08f992"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/akhenakh/vduck/releases/download/v1.2.3/vduck_Linux_arm64.tar.gz"
-      sha256 "d9e09189a61bbc8bb23dbd4ac32e765c35e8038c04b7a624cf4937b85d5a7ce8"
+      url "https://github.com/akhenakh/vduck/releases/download/v1.2.4/vduck_Linux_arm64.tar.gz"
+      sha256 "4b2baad9c97fe875889235b0548c0869e957cc2bbd24ee1bbcf8858e4935d677"
     end
     on_intel do
-      url "https://github.com/akhenakh/vduck/releases/download/v1.2.3/vduck_Linux_x86_64.tar.gz"
-      sha256 "4b36fa544b3da62d65da90d7f00f1d814f165aae27ae7dc681322a47103029ec"
+      url "https://github.com/akhenakh/vduck/releases/download/v1.2.4/vduck_Linux_x86_64.tar.gz"
+      sha256 "dcb5bdc36e2ddcf88e3317ab8ca3ac91e6aa5c7a200f22989106be0ea24de4b7"
     end
   end
 
