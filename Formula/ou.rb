977e523a103bd61196e4ccf78dd259aa6c558c77
Fabrice Aneche <akh@inair.space>
2026-08-24 14:52:30 +0000

    Brew formula update for ou v0.1.4

--- Formula/ou.rb
+++ Formula/ou.rb
@@ -8,23 +8,23 @@ class Ou < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/akhenakh/ou/releases/download/v0.1.3/ou_Darwin_arm64.tar.gz"
-      sha256 "704f1b278d54a928d693971d4c9ecc2a25f13589a03d36d5b99a512728ebd139"
+      url "https://github.com/akhenakh/ou/releases/download/v0.1.4/ou_Darwin_arm64.tar.gz"
+      sha256 "6163e4f74ea099f565233fdedcb734d2ba24b730b45be4230eb34518444697da"
     end
     on_intel do
-      url "https://github.com/akhenakh/ou/releases/download/v0.1.3/ou_Darwin_x86_64.tar.gz"
-      sha256 "b03ae87ea1185def3ff9d3d4d6cfb43735c2da44caddde307028cd9f229d2fb4"
+      url "https://github.com/akhenakh/ou/releases/download/v0.1.4/ou_Darwin_x86_64.tar.gz"
+      sha256 "a300d59b6895730b761e22322a679a164bb322421df918cf8586de0391f420d8"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/akhenakh/ou/releases/download/v0.1.3/ou_Linux_arm64.tar.gz"
-      sha256 "90b78bef89dbd4207ac9f290cac0d2d7cc9576332efcdba591e2da687fe80c13"
+      url "https://github.com/akhenakh/ou/releases/download/v0.1.4/ou_Linux_arm64.tar.gz"
+      sha256 "57626c45972c9b5cd0e252395e27cbe17aa9113a8103eecf263c014f2bd07e47"
     end
     on_intel do
-      url "https://github.com/akhenakh/ou/releases/download/v0.1.3/ou_Linux_x86_64.tar.gz"
-      sha256 "4b030df9e38091afa9f2ba41d30eb69387a39294b2fd7b1252b73649f48cd970"
+      url "https://github.com/akhenakh/ou/releases/download/v0.1.4/ou_Linux_x86_64.tar.gz"
+      sha256 "504b9ef1d2116fc51480643f7c5d61b4127217852897c8c418cf56aab73f37e6"
     end
   end
 
