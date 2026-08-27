af3c58d0d84c56cc67059188b2afbe2627404aad
Fabrice Aneche <akh@inair.space>
2026-08-27 17:20:59 +0000

    Brew formula update for wpail v0.1

---
+++ Formula/wpail.rb
@@ -0,0 +1,30 @@
+# typed: false
+# frozen_string_literal: true
+
+class Wpail < Formula
+  desc "Find what application is listening on a TCP/UDP port"
+  homepage "https://github.com/akhenakh/wpail"
+  license "MIT"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/akhenakh/wpail/releases/download/v0.1/wpail_Darwin_arm64.tar.gz"
+      sha256 "041d7981de3f7761b786ea680c904e86380efce7eaef7d97ab1ca2d281758dec"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/akhenakh/wpail/releases/download/v0.1/wpail_Linux_arm64.tar.gz"
+      sha256 "f4fce20bc837ebe73737e45ba0a34a95322db2b40ceb3ab572554ac5642798a6"
+    end
+    on_intel do
+      url "https://github.com/akhenakh/wpail/releases/download/v0.1/wpail_Linux_x86_64.tar.gz"
+      sha256 "e824295bd9402625fbf46a2fca0d04caaf0c49da47a797910ae277a36d54b437"
+    end
+  end
+
+  def install
+    bin.install "wpail"
+  end
+end
