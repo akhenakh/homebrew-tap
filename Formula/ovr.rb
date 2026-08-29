83a6e624a3fe3321f043cb6a3b6e62249e6cba9e
Fabrice Aneche <akh@inair.space>
2026-08-28 23:39:41 -0400

    Brew formula update for ovr v0.1

--- Formula/ovr.rb
+++ Formula/ovr.rb
@@ -10,6 +10,11 @@ class Ovr < Formula
     on_arm do
       url "https://github.com/akhenakh/ovr/releases/download/v0.1/ovr_Darwin_arm64.tar.gz"
       sha256 "b725f4a50666d444c08ff08d819c3aeaac859fa4d989d2cd6f87452b2928cfdd"
+      resource "ovrui" do
+        version "v0.1"
+        url "https://github.com/akhenakh/ovr/releases/download/v0.1/ovrui_Darwin_arm64.tar.gz"
+        sha256 "a140ffa2ddf76d3b55d7b20214999a918b5b14ebb0836af95cf994c9fe24d78d"
+      end
     end
     on_intel do
       url "https://github.com/akhenakh/ovr/releases/download/v0.1/ovr_Darwin_x86_64.tar.gz"
@@ -21,16 +26,6 @@ class Ovr < Formula
     on_arm do
       url "https://github.com/akhenakh/ovr/releases/download/v0.1/ovr_Linux_arm64.tar.gz"
       sha256 "e167aca74ca900cca3f18ad4980eacf7d041d67d22b883ec5015cdc2a244c65e"
-    end
-    on_intel do
-      url "https://github.com/akhenakh/ovr/releases/download/v0.1/ovr_Linux_x86_64.tar.gz"
-      sha256 "68934bb7c59d69ff01b2222c05c09791d95cee3bedbc7062a304b577748692e0"
-    end
-  end
-
-  # ovrui ships in its own archives, as a resource
-  on_linux do
-    on_arm do
       resource "ovrui" do
         version "v0.1"
         url "https://github.com/akhenakh/ovr/releases/download/v0.1/ovrui_Linux_arm64.tar.gz"
@@ -38,6 +33,8 @@ class Ovr < Formula
       end
     end
     on_intel do
+      url "https://github.com/akhenakh/ovr/releases/download/v0.1/ovr_Linux_x86_64.tar.gz"
+      sha256 "68934bb7c59d69ff01b2222c05c09791d95cee3bedbc7062a304b577748692e0"
       resource "ovrui" do
         version "v0.1"
         url "https://github.com/akhenakh/ovr/releases/download/v0.1/ovrui_Linux_x86_64.tar.gz"
@@ -46,25 +43,12 @@ class Ovr < Formula
     end
   end
 
-  on_macos do
-    on_arm do
-      resource "ovrui" do
-        version "v0.1"
-        url "https://github.com/akhenakh/ovr/releases/download/v0.1/ovrui_Darwin_arm64.tar.gz"
-        sha256 "a140ffa2ddf76d3b55d7b20214999a918b5b14ebb0836af95cf994c9fe24d78d"
-      end
-    end
-  end
-
   def install
     bin.install "ovr"
-    on_linux do
+    if OS.mac? && Hardware::CPU.arm?
       resource("ovrui").stage { bin.install "ovrui" }
-    end
-    on_macos do
-      on_arm do
-        resource("ovrui").stage { bin.install "ovrui" }
-      end
+    elsif OS.linux?
+      resource("ovrui").stage { bin.install "ovrui" }
     end
   end
 end
